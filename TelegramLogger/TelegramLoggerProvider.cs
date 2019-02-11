f5ec7e870c40d9a54db35452fdcb16ac28c6a2b2
Chacaroon <nekit2326@gmail.com>
2019-02-11 14:00:51 +0200

    Add TelegramLogger classlib and service for testinf

---
+++ TelegramLogger/TelegramLoggerProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramLogger
+{
+	class TelegramLoggerProvider : ILoggerProvider
+	{
+		private string _token;
+		private LogLevel _logLevel;
+
+		public TelegramLoggerProvider(string token, LogLevel logLevel)
+		{
+			_token = token;
+			_logLevel = logLevel;
+		}
+
+		public ILogger CreateLogger(string categoryName)
+		{
+			return new TelegramLogger(_token, _logLevel);
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
